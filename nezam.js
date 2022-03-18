e0abe0f7fefcae49e1d96896ecd39a99e1b1e0a3
datastory <honza@datastory.cz>
2022-03-18 16:33:03 +0100

    graf, co zatim nic neukazuje

---
+++ nezam.js
@@ -0,0 +1,56 @@
+Highcharts.setOptions({
+    lang: {
+      numericSymbols: [' tis.', ' mil.', 'mld.', 'T', 'P', 'E'],
+    },
+  });
+
+fetch('./nezam.json')
+  .then(response => response.json())
+  .then(d => {
+    const upd = Object.keys(d).slice(-1)[0].split('-');
+    Highcharts.chart('graf_nezam', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Lidé aktuálně hledající práci'
+        },
+        subtitle: {
+            text: `aktuální k ${parseInt(upd[2])}. ${parseInt(upd[1])}. ${upd[0]}`,
+            useHTML: true
+        },
+        credits: {
+            href : 'https://www.czso.cz/csu/czso/uchazeci-o-zamestnani-dosazitelni-a-podil-nezamestnanych-osob-podle-obci_090417',
+            text : 'Dosažitelní uchazeči o zaměstnání, ČSÚ'
+        },
+        xAxis: {
+            categories: Object.keys(d).map((v) => `${parseInt(v.split('-')[1])}. ${v.split('-')[0]}`)
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'počet osob'
+            }
+        },
+        plotOptions: {
+            series: {
+                stacking: 'normal',
+                animation: false,
+            }
+        },
+        tooltip: {
+            shared: true,
+        },
+        series: [{
+            name: 'ženy',
+            color: '#e41a1c',
+            data: Object.values(d).map((v) => v.z)
+        }, {
+            name: 'muži',
+            color: '#377eb8',
+            data: Object.values(d).map((v) => v.m)
+        }]
+    });
+  });
+
+
---
+++ tools/prep.py
@@ -0,0 +1,33 @@
+# %%
+import json
+import pandas as pd
+# %%
+d = pd.read_csv('https://www.czso.cz/documents/62353418/171347245/250169-22data031122.zip/a6923d2a-61e0-450b-92bf-6fccfea8c526?version=1.1', compression='zip').append(pd.read_csv('https://www.czso.cz/documents/62353418/143520468/250169-21data011122.zip/338d1c6d-a9aa-4313-835e-8a42af93402b?version=1.1', compression='zip'))
+# %%
+def dt(v):
+    v = str(int(v))
+    return v[0:4] + '-' + v[4:6] + '-' + v[6:8]
+# %%
+sub = list(d[d.vuk_text.isin([
+    'Uchazeči o zaměstnání dosažitelní',
+    'Uchazeči o zaměstnání  dosažitelní - ženy',
+])].groupby(['obdobi', 'vuk_text']).hodnota.sum().reset_index().to_dict(orient='index').values())
+
+out = {}
+for row in sub:
+    dte = dt(row['obdobi'])
+    if dte not in out:
+        out[dte] = {}
+    if 'ženy' in row['vuk_text']:
+        out[dte]['z'] = row['hodnota']
+    else:
+        out[dte]['m'] = row['hodnota']
+
+for row in out.values():
+    row['m'] = row['m'] - row['z']
+
+    
+# %%
+with open('../nezam.json', 'w') as f:
+    f.write(json.dumps(out, ensure_ascii=False))
+# %%
